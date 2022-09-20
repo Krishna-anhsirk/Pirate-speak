257c1e7aa9660d3eac7468a00937e51482bd0327
Krishna-anhsirk <Krishnatejajinka@gmail.com>
2022-09-20 10:24:28 +0530

    functionality done with mock server

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 var inputText = document.querySelector("#input-txt");
+var translateText = document.querySelector("#btn-translate");
+var outputText = document.querySelector("#output");
 
-console.log(inputText.value);
+function urlConstructor(text) {
+  return (
+    "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=" + text
+  );
+}
+
+function doTranslate(text) {
+  var url = urlConstructor(text);
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => (outputText.innerText = data.contents.translated))
+    .catch();
+}
+
+translateText.addEventListener("click", () => {
+  doTranslate(inputText.value);
+});
